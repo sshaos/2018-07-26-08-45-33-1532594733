bdc8eb80d221b23be5b7b8999ecea37bcebf67d5
shaoshuai <shaoshuai@hust.edu.cn>
2018-07-28 22:22:01 +0800

    change mian funcation

--- main/main.js
+++ main/main.js
@@ -1,21 +1,35 @@
 // Write your cade below:
 module.exports = function main(a,b) {
-	input_Check(a,b);
+	var flag = input_Check(a,b);
+	if(flag)
+	{
 	var c=a%b;
 	return c;
+	}
+	else
+	return;
 };
-
-//1.检查输入a，b是否是非负整数范围内
-//2.是否兼容其他进制数字
-function input_Check(a,b){
+//-----检查输入是否合法函数------//
+//1.检查数据类型是否是number
+//2.检查输入a，b是否是非负整数范围内(对浮点数取余数容易产生数据错误)
+function input_Check(a,b)
+{
 	var typeof_a = typeof(a);
 	var typeof_b = typeof(b);
 	if(typeof_a != "number"||typeof_b != "number" )
 	{
-		console.error("Please input number！\n");
+		console.error("Please enter number！\n");
+		return false;
 	}
 	if(a<0||b<0)
 	{
-		console.error("Please input number！\n");
+		console.error("Please enter non-negative number！\n");
+		return false;
 	}
+	if(!(Number.isInteger(a)&&Number.isInteger(b)))
+	{
+		console.error("Please enter integers！\n");
+		return false;
+	}
+	return true;
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -5,6 +5,5 @@ describe('main()', () => {
     it('should calculate the remaindar', () => {
         expect(main(9, 3)).toBe(0);
         expect(main(11, 4)).toBe(3);
-        //expect(main('asad', 4)).toBe(3);
     });
 });
