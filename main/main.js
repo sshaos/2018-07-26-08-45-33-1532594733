2f1d64dae65477eab109f9a43f8c57c0b517f803
shaoshuai <shaoshuai@hust.edu.cn>
2018-07-28 16:51:25 +0800

    npm install+change main

--- main/main.js
+++ main/main.js
@@ -1,4 +1,21 @@
 // Write your cade below:
-module.exports = function main() {
-  
+module.exports = function main(a,b) {
+	input_Check(a,b);
+	var c=a%b;
+	return c;
+};
+
+//1.检查输入a，b是否是非负整数范围内
+//2.是否兼容其他进制数字
+function input_Check(a,b){
+	var typeof_a = typeof(a);
+	var typeof_b = typeof(b);
+	if(typeof_a != "number"||typeof_b != "number" )
+	{
+		console.error("Please input number！\n");
+	}
+	if(a<0||b<0)
+	{
+		console.error("Please input number！\n");
+	}
 };
